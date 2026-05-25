920c94a53543fae46733e7e00f2c4c04eeee2055
Shashwat023 <avaiverma2004@gmail.com>
2026-05-25 17:02:18 +0530

    Configure preDeployCommand and NODE_VERSION for Render build environment

--- build.sh
+++ build.sh
@@ -7,9 +7,3 @@ pip install -r requirements.txt
 
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
-
-echo "Running migrations..."
-python manage.py migrate
-
-echo "Seeding database with sample data..."
-python manage.py seed_data
